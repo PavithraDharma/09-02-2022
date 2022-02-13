d7caf3bc0ff30b6ad410bc9aac13eb4f054e90c1
Pavithra <Pavithra@LAPTOP-ECJUFKE0>
2022-02-13 17:44:55 +0530

    9Feb Commit

---
+++ src/com/demo/array.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class array
+{
+public static void main (String  args[])
+{
+int a[]=new int[3];
+a[0]=10; a[1]=20; a[2]=30;
+for(int i=0;i<=2;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+
